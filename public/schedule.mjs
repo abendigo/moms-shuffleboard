d5435cd73dc7a50a7fb64c50f3e0f17ddee5170f
Mark Oosterveld <mark.oosterveld@indexexchange.com>
2018-08-20 14:37:38 -0400

    refactor

--- public/schedule.mjs
+++ public/schedule.mjs
@@ -1,43 +1,9 @@
-
-
 export const Schedule = {
-  data: function() {
-    return {
-      timeslots: ['13:00', '14:00', '15:00'],
-      dates: [
-        'Sep 24', 'Oct 01', 'Oct 08', 'Oct 15', 'Oct 22',
-        'Oct 29', 'Nov 05', 'Nov 12', 'Nov 19', 'Nov 26']
-    };
-  },
-  computed: Vuex.mapState(['schedule', 'teams']),
+  computed: Vuex.mapState(['timeslots', 'dates', 'schedule', 'teams']),
   methods: {
     getTeam: function(id) {
       return this.teams[id] || {};
     }
   },
-  template: `
-<table style="border: 4px solid black;">
-<thead>
-  <tr>
-    <th></th>
-    <th v-for="timeslot in timeslots">{{timeslot}}</th>
-  </tr>
-</thead>
-<tbody>
-  <tr v-for="date in dates">
-    <td>{{date}}</td>
-    <td v-for="timeslot in timeslots">
-      <ul>
-        <li v-for="team in schedule[date][timeslot]">
-          <ol>
-            <li>{{ getTeam(team).member_1 }}</li>
-            <li>{{ getTeam(team).member_2 }}</li>
-          </ol>
-        </li>
-      </ul>
-    </td>
-  </tr>
-</tbody>
-</table>
-`
+  template: '#schedule-template'
 };
--- public/teams.mjs
+++ public/teams.mjs
@@ -1,1 +1,78 @@
-export const Teams = { template: '<div>teams</div>' }
+export const Teams = {
+  data: function() {
+    return {
+      form: {
+        member_1: '',
+        member_2: ''
+      }
+    };
+  },
+  computed: Vuex.mapState(['timeslots', 'dates', 'schedule', 'teams']),
+  methods: {
+    processForm: function() {
+      console.log('processForm', this.form.member_1, this.form.member_2);
+
+      firestore.collection('teams').add({
+        member_1: this.form.member_1,
+        member_2: this.form.member_2
+      }).then(ref => {
+        console.log('success', ref);
+      }, error => {
+        console.log('error', error);
+      });
+    },
+    onDelete: function(id) {
+      console.log('onDelete', id)
+
+      firestore.collection('teams').doc(id).delete().then(() => {
+        console.log('deleted')
+      }, error => {
+        console.log('error', error)
+      })
+    },
+    shuffleArray(array) {
+      for (let i = array.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [array[i], array[j]] = [array[j], array[i]];
+      }
+    },
+    generateSchedule: function() {
+      console.log('generateSchedule', this.teams)
+
+      let schedule = {};
+
+      const slotCount = this.timeslots.length;
+      for (let date of this.dates) {
+        schedule[date] = {};
+
+        // get a list of keys
+        let ids = Object.keys(this.teams);
+        this.shuffleArray(ids);
+
+        // randomize starting location, incase the number of teams
+        // is not evenly divisible
+        let slotIndex = Math.floor(Math.random() * slotCount);
+        // console.log(date, 'slot', slotIndex)
+
+        for (let id of ids) {
+          let timeslot = this.timeslots[slotIndex];
+          // console.log('---', id, slotIndex, timeslot)
+
+          if (!schedule[date][timeslot])
+            schedule[date][timeslot] = [];
+          schedule[date][timeslot].push(id);
+
+          slotIndex = (slotIndex + 1) % slotCount;
+        }
+
+        // console.log('==================')
+      }
+
+      // console.log('schedule', schedule)
+      // this.schedule = schedule;
+      this.$store.commit('setSchedule', schedule);
+    }
+  },
+
+  template: '#teams-template'
+}
